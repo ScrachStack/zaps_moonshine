1035de481d84c0b416d78b826a1391d9ac19aafb
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-15 22:46:17 -0700

    Update framework.lua

--- server/framework.lua
+++ server/framework.lua
@@ -1,8 +1,20 @@
+if GetResourceState('vorp_core') ~= 'missing' then
+    vorp = false 
+else 
+    vorp = true
+end
+if vorp then 
 local VORPcore = {}
 
 TriggerEvent("getCore", function(core)
     VORPcore = core
 end)
 Inventory = exports.vorp_inventory:vorp_inventoryApi()
+end
 
---RedEM = exports["redem_roleplay"]:RedEM()
+if GetResourceState('redem_roleplay') ~= 'missing' then
+    redem = false
+else 
+    Core = exports["redem_roleplay"]:RedEM()
+    redem = true
+end
