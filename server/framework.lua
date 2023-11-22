7df76908a29b7e72b74ce7bb0641a1f04e78c1cd
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-11-21 22:10:30 -0600

    Update framework.lua

--- server/framework.lua
+++ server/framework.lua
@@ -10,6 +10,7 @@ TriggerEvent("getCore", function(core)
     VORPcore = core
 end)
 Inventory = exports.vorp_inventory:vorp_inventoryApi()
+    print('Framework: VORP')
 end
 
 if GetResourceState('redem_roleplay') ~= 'missing' then
